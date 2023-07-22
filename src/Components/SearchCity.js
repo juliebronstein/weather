fffca97a9204413d00891aa99af4b2bf8b7bf2c6
samanta580 <samaneh.azhar@gmail.com>
2023-07-22 19:56:37 +0330

    commit 4

--- src/Components/SearchCity.js
+++ src/Components/SearchCity.js
@@ -1,84 +1,105 @@
-import perfectDay from '../icons/perfect-day.svg';
-import InputGroup from 'react-bootstrap/InputGroup';
-import Form from 'react-bootstrap/Form';
-import { useNavigate } from 'react-router-dom';
-import { useCallback, useEffect, useState } from 'react';
+import perfectDay from "../icons/perfect-day.svg";
+import InputGroup from "react-bootstrap/InputGroup";
+import Form from "react-bootstrap/Form";
+import { useNavigate } from "react-router-dom";
+import { useCallback, useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import axios from 'axios';
-import nameApp from '../utils/img/name.png'
-import logo from '../utils/img/github.png'
-import Weather from './Weather';
-import Forecast from './Forecast';
-import NotFound from './NotFound';
-import Loader from './Loader';
+import axios from "axios";
+import nameApp from "../utils/img/name.png";
+import logo from "../utils/img/github.png";
+import Weather from "./Weather";
+import Forecast from "./Forecast";
+import NotFound from "./NotFound";
+import Loader from "./Loader";
 
 const SearchCity = () => {
-    // const apiKey="fe4feefa8543e06d4f3c66d92c61b69c"
-     // const api ="https://api.openweathermap.org/data/2.5/weather?q="
-    const apiKey="&key=N9LUTKS5U4DFC6MCLWYAW9SF2"
-    const api ="https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"
-    const unitGroup="?unitGroup=us"
-    const type="&contentType=json"
-    const history=useNavigate();
-    const [weather,setWeather]= useState(null);
-    const [notFound,setNotFound]= useState(0);
-    const [city,setCity] = useState(null);
-    const time = new Date().toDateString()
-// const isDayTime = hours > 6 && hours < 20
-    // useEffect(() => {
-    //     foreCasting()
-    // }, [city])
-    
-    // const foreCasting=()=>{
-    //     const rest=axios.get(`${api}${city}${unitGroup}${apiKey}${type}`).then(res=>
-    //     console.log("restiiiiii:",res))
-    // }
-    const getWeather = useCallback(async()=> {
-        try {
-              const res =await axios.get(`${api}${city}${unitGroup}${apiKey}${type}`);
-              setWeather(res.data);
-              console.log(res);
-            }
-        catch (err) {
-              console.log(err);
-              setNotFound(1);
-          }
-        },[city])
-    const submit = (e)=> {
-        e.preventDefault();
-        getWeather();
-        history("/weather");
+  // const apiKey="fe4feefa8543e06d4f3c66d92c61b69c"
+  // const api ="https://api.openweathermap.org/data/2.5/weather?q="
+  const apiKey = "&key=N9LUTKS5U4DFC6MCLWYAW9SF2";
+  const api =
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/";
+  const unitGroup = "?unitGroup=us";
+  const type = "&contentType=json";
+  const history = useNavigate();
+  const [weather, setWeather] = useState(null);
+  const [notFound, setNotFound] = useState(0);
+  const [city, setCity] = useState(null);
+  const today = new Date();
+  var date =
+    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+  var time = today.getHours() + ":" + today.getMinutes();
+  var zoneMin = today.getTimezoneOffset()%60;
+  var zoneHur = Math.round(today.getTimezoneOffset() / 60);
+  var zone=""
+  if (zoneHur < 0) {
+    zoneHur = zoneHur * -1;
+    zoneMin = zoneMin * -1;
+     zone ="-"+ zoneHur + ":" + zoneMin +"GMT";
+  }
+  if (zoneHur > 0)zone = "+"+ zoneHur + ":" + zoneMin +"  GMT";
+  console.log(zone);
+;
+  var dateTime = date + "  " + time+"    "+zone;
+  const getWeather = useCallback(async () => {
+    try {
+        console.log(`${api}${city}${unitGroup}${apiKey}${type}`)
+      const res = await axios.get(`${api}${city}${unitGroup}${apiKey}${type}`);
+      setWeather(res.data);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+      setNotFound(1);
     }
-    useEffect(()=> {
-        setWeather(null);
-        setNotFound(0);
-    },[]);
-    return (
-        <>
-         <div className='fs-4 navbar-brand'>
-           <div className='app-name'> <img src={nameApp} alt="Internet connection is poor"/> </div>
-           <div className='date'>{time}</div>
-           <div className='app-logo'><a href='/#'> <img className='logo' src={logo} alt="Internet connection is poor"/> </a> </div>
-            </div>
-            <form onSubmit={submit}>
-                <InputGroup className="mt-4 mb-2">
-                    <Form.Control
-                    placeholder="City"
-                    aria-label="City"
-                    onChange={(e)=> setCity(e.target.value)}
-                    />
-                    <button className='my-btn'> Search</button>
-                </InputGroup>
-            </form>
-            {weather?   
-            <div className='d-flex justify-content-center flex-column flex-lg-row '>
-            <Weather weather={weather} notFound={notFound}/>
-            <Forecast weather={weather} notFound={notFound}/>
-            </div>:  (
-            notFound ? <NotFound/>:<Loader/>
-        ) }
-         
-        </>
-
-)}
+  }, [city]);
+  const submit = (e) => {
+    e.preventDefault();
+    getWeather();
+    history("/weather");
+  };
+  useEffect(() => {
+    setWeather(null);
+    setNotFound(0);
+  }, []);
+  return (
+    <>
+      <div className="fs-4 navbar-brand">
+        <div className="app-name">
+          {" "}
+          <img src={nameApp} alt="Internet connection is poor" />{" "}
+        </div>
+        <div className="date header">{dateTime}</div>
+        <div className="app-logo">
+          <a href="/#">
+            {" "}
+            <img
+              className="logo"
+              src={logo}
+              alt="Internet connection is poor"
+            />{" "}
+          </a>{" "}
+        </div>
+      </div>
+      <form onSubmit={submit}>
+        <InputGroup className="mt-4 mb-2">
+          <Form.Control
+            placeholder="City"
+            aria-label="City"
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <button className="my-btn"> Search</button>
+        </InputGroup>
+      </form>
+      {weather ? (
+        <div className="d-flex justify-content-center flex-column flex-lg-row ">
+          <Weather weather={weather} notFound={notFound} />
+          <Forecast weather={weather} notFound={notFound} />
+        </div>
+      ) : notFound ? (
+        <NotFound />
+      ) : (
+        <Loader />
+      )}
+    </>
+  );
+};
 export default SearchCity;
--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,11 +1,11 @@
 import { motion } from "framer-motion";
-import Loader from './Loader';
-import NotFound from './NotFound';
+import Loader from "./Loader";
+import NotFound from "./NotFound";
 // import images
-import sunset from '../icons/temp.svg';
-import humidity from '../icons/humidity.svg';
-import wind from '../icons/wind.svg';
-import pressure from '../icons/pressure.svg';
+import sunset from "../icons/temp.svg";
+import humidity from "../icons/humidity.svg";
+import wind from "../icons/wind.svg";
+import pressure from "../icons/pressure.svg";
 import sunny from "../icons/sunny.svg";
 import night from "../icons/night.svg";
 import day from "../icons/day.svg";
@@ -18,87 +18,123 @@ import storm from "../icons/storm.svg";
 import smoke from "../icons/smoke.svg";
 import mist from "../icons/mist.svg";
 
+const Weather = ({ weather }) => {
+  const addr=weather.resolvedAddress.split(",")
+const weatherH=weather?.currentConditions.datetime.split(":")
+const weatherSunrise=weather?.currentConditions?.sunrise.split(":")
+const weatherSunset=weather?.currentConditions?.sunset.split(":")
+const icon=weather.currentConditions.icon
+const temp=((weather.currentConditions.temp-32)*5/9).toPrecision(4) 
+const isDay = (weatherH[0]>weatherSunrise[0] && weatherH[0]<weatherSunset[0])?"isDay":"isNight"
+console.log("icon",icon)
 
-const Weather = ({weather,notFound}) => {
-    // const isDay = "isDay"
+  const weatherVariants = {
+    hidden: {
+      x: "1000vh",
+    },
+    visible: {
+      x: 0,
+      transition: { duration: 1.5, type: "spring" },
+    },
+  };
+  // var weatherInfo = [
+  //     {id:1 , name:(isDay)?'sunset':'sunrise' , val:sunset , num:`${getTime(weather?.sys[isDay ? "sunset" : "sunrise"])}`},
+  //     {id:2 , name:'humidity' , val:humidity , num:weather?.main.humidity},
+  //     {id:3 , name:'wind' , val:wind , num:weather?.wind.speed},
+  //     {id:4 , name:'pressure' , val:pressure , num:weather?.main.pressure }
+  // ];
+  const WeatherIcons = {
+    "01d": sunny,
+    "01n": night,
+    "02d": day,
+    "02n": cloudyNight,
+    "03d": cloudy,
+    "03n": cloudy,
+    "04d": perfectDay,
+    "04n": cloudyNight,
+    "09d": rain,
+    "09n": rainNight,
+    "partly-cloudy-day": rain,
+    "10n": rainNight,
+    "11d": storm,
+    "11n": storm,
+    "50d": smoke,
+    "50n": mist,
+  };
 
-    // const getTime = (time) => {
-    //     return `${new Date(time * 1000).getHours()} : ${new Date(time * 1000).getMinutes()}`;
-    // }
-    const weatherVariants = {
-        hidden: { 
-            x: '1000vh', 
-        },
-        visible: { 
-            x: 0, 
-            transition: { duration: 1.5 ,type:'spring'}
-        },
-    };
-    // var weatherInfo = [
-    //     {id:1 , name:(isDay)?'sunset':'sunrise' , val:sunset , num:`${getTime(weather?.sys[isDay ? "sunset" : "sunrise"])}`},
-    //     {id:2 , name:'humidity' , val:humidity , num:weather?.main.humidity},
-    //     {id:3 , name:'wind' , val:wind , num:weather?.wind.speed},
-    //     {id:4 , name:'pressure' , val:pressure , num:weather?.main.pressure }
-    // ];
-    const WeatherIcons = {
-        "01d":sunny,
-        "01n":night,
-        "02d":day,
-        "02n":cloudyNight,
-        "03d":cloudy,
-        "03n":cloudy,
-        "04d":perfectDay,
-        "04n":cloudyNight,
-        "09d":rain,
-        "09n":rainNight,
-        "10d":rain,
-        "10n":rainNight,
-        "11d":storm,
-        "11n":storm,
-        "50d":smoke,
-        "50n":mist,
-    }
-    
-    weather && console.log(weather);
-    //! media query
-    return ( 
-      
-            <motion.div 
-            className="weather-content me-lg-1 mb-sm-1 col-12  col-lg-6 d-flex flex-column  justify-content-center text-center p-3 g-3"
-            variants={weatherVariants}
-            initial='hidden'
-            animate='visible'
-            >
+  weather && console.log(weather);
+  //! media query
+  return (
+    <motion.div
+      className="weather-content me-lg-1 mb-sm-1 col-12  col-lg-6 d-flex flex-column  justify-content-center text-center p-3 g-3"
+      variants={weatherVariants}
+      initial="hidden"
+      animate="visible"
+    >
+      <div className="d-flex flex-column justify-content-center ">
+        <div className="header">CURRENT WEATHER</div>
+        <div className="d-flex flex-column flex-md-row  justify-content-around col-12 title mb-4 mt-2  ">
 
-                    <div className="d-flex flex-column justify-content-center ">
-                        <div className="header">CURRENT WEATHER</div>
-                      <div className="d-flex flex-row justify-content-center title ">
-                      <div>1</div>
-                        <div>2</div>
-                        <div>3</div>
-                      </div>
-                    </div>
-                    <div className="d-flex flex-column justify-content-center ">
-                        <div className="header">AIR CONDITIONS</div>
-                      <div className="d-flex flex-row justify-content-center title">
-                      <div>1</div>
-                        <div>2</div>
-                        <div>3</div>
-                        <div>3</div>
-                      </div>
-                    </div>
-                    <div className="d-flex flex-column justify-content-center ">
-                        <div className="header">TODAY'S FORECAST</div>
-                        <div className="subheader">3 available forecasts</div>
-                      <div className="d-flex flex-row justify-content-center title">
-                      <div>1</div>
-                        <div>2</div>
-                        <div>3</div>
-                      </div>
-                    </div>
-                  
 
-            {/* <h3 className='fs-4 fw-bold'>React Weather App</h3>
+          <div className="d-flex flex-row flex-lg-column  justify-content-center  col-lg-4">
+            <div className="title header-font">{addr[0]},{addr[1]}</div>
+            <div className="header title-font">{weatherH[0]}:{weatherH[1]}</div>
+          </div>
+       
+
+          <div className="d-flex flex-row flex-lg-column justify-content-center  col-lg-4">
+            <div className="title">{temp}°C</div>
+            <div className="header">{weather.currentConditions.conditions}</div>
+          </div>
+       
+
+          <div className="d-flex flex-row flex-lg-column justify-content-center  col-lg-4">
+          <img className='weather-img' src={WeatherIcons[weather.currentConditions.icon]} alt="" />
+          {console.log(WeatherIcons[weather.currentConditions.icon])}
+           
+          </div>
+       
+
+
+        </div>
+      </div>
+      <div className="d-flex flex-column justify-content-center mb-3 mt-3  ">
+        <div className="header">AIR CONDITIONS</div>
+        <div className="d-flex flex-column flex-md-row  justify-content-around col-12 title mb-4 mt-2 ">
+        <div className="d-flex flex-row flex-lg-column  justify-content-center  col-lg-3">
+            <div className="header">1dfgdfg</div>
+            <div className="title">1fgdf</div>
+          </div>
+       
+
+          <div className="d-flex flex-row flex-lg-column justify-content-center  col-lg-3">
+            <div className="header">1dfgdfg</div>
+            <div className="title">1fgdf</div>
+          </div>
+       
+
+          <div className="d-flex flex-row flex-lg-column justify-content-center  col-lg-3">
+            <div className="header">1dfgdfg</div>
+            <div className="title">1fgdf</div>
+          </div>
+          <div className="d-flex flex-row flex-lg-column justify-content-center  col-lg-3">
+            <div className="header">1dfgdfg</div>
+            <div className="title">1fgdf</div>
+          </div>
+       
+        </div>
+      </div>
+      <div className="d-flex flex-column justify-content-center mb-3 mt-2  ">
+        <div className="header">TODAY'S FORECAST</div>
+        <div className="subheader">3 available forecasts</div>
+        <div className="d-flex flex-row justify-content-center title">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+      </div>
+
+      {/* <h3 className='fs-4 fw-bold'>React Weather App</h3>
             <div className="temp d-flex justify-content-between align-items-center my-4">
                 <div className='fs-5'> <span className='fs-2'>{`${Math.floor(weather?.currentConditions?.temp - 273)}°C`}</span>|{weather.currentConditions.conditions}</div>
                 <img className='weather-img' src={WeatherIcons[weather.currentConditions.icon]} alt="" />
@@ -106,7 +142,7 @@ const Weather = ({weather,notFound}) => {
             <span className="city-info fs-2 fw-bold my-2">{weather.resolvedAddress}</span>
             <span className="text-start my-2">Weather info</span>
             <div className="weather-info d-flex flex-wrap w-100 text-center"> */}
-                {/* {weatherInfo.map(ele=> (
+      {/* {weatherInfo.map(ele=> (
                     <div className='d-flex justify-content-center align-items-center p-2 w-50' key={ele.id}>
                         <img className=' img-fluid me-3' src={ele.val} alt="" />
                         <div className="d-flex flex-column justify-content-center align-items-center g-2">
@@ -116,8 +152,7 @@ const Weather = ({weather,notFound}) => {
                     </div>
                 ))}
             </div> */}
-            </motion.div>
-      
-    );
-}
+    </motion.div>
+  );
+};
 export default Weather;
